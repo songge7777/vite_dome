83120bca44845a28a80b3dc0731dc32002cee170
songge <gege.song@hbccy.com.cn>
2022-12-30 10:18:41 +0800

    fix hash 模式

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import "antd/dist/reset.css";
 
-import { BrowserRouter  } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import "@/styles/index.scss";
 
 import App from "@/router/index";
 console.log("basename={import.meta.env.BASE_URL}",import.meta.env.BASE_URL);
 ReactDOM.createRoot( document.getElementById("root")! ).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
